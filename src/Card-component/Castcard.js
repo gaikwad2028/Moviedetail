ad5e99f8159a5d71fcec680f64fc3ca3991b19a1
gaikwad2028 <108988732+gaikwad2028@users.noreply.github.com>
2022-11-26 22:30:30 +0530

    border

--- src/Card-component/Castcard.js
+++ src/Card-component/Castcard.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Castcard(props) {
   return (
     <div className="card1_details">
-  <img className="card-img-top1" src={props.Image1} alt="Card image cap"/>
+  <img className="card-img-top1" src={props.Image1} alt="Card images cap"/>
   <div className="card-body1">
     <p className="card-text1">{props.Titel}</p>
-    <p className='card-rating1' style={{margin:"0"}}> Character:{props.Role}</p>
+    <p className='card-rating1' style={{margin:"0"}}> <span className='charat'>Character:-</span><span className='role'>{props.Role}</span></p>
   </div>
 </div>
   )
--- src/Card-component/DetailCard.js
+++ src/Card-component/DetailCard.js
@@ -7,25 +7,25 @@ export default function DetailCard(props) {
             <div className='container'>
                <div className='detail-imgcontainer'>
                 <div className='detail-img'>
-                <img className='detail_imgs' src={props. Cardimg} alt="cardimg"></img>
+                <img className='detail_imgs' src={props.Cardimg} alt="cardimg"></img>
                 </div>
                 <div className='detail-info'>
                     <div className='detail_h1'><h1 className='detail_h1'>{props.Titel}</h1> <p>{props.Tagline}</p></div>
-                    <h2 className='rating' style={{color:"green"}}>Rating:-{props.Rating}</h2>
+                    <h2 className='rating' style={{color:"white"}}> <span className='rating1'>Rating</span>:- <span className='rat_num'>{props.Rating}</span></h2>
                     <p className='detail-time'>
                        <span className='minutes'>{props.Runtime}min</span>
                        <span className='detail_type'>scince,action,thriller</span>
                     </p>
-                    <div className='deatil_infofooter'><p> Release Date:{props.Date}</p></div>
+                    <div className='deatil_infofooter'><p> Release Date:- <span className='date'>{props.Date}</span></p></div>
                 </div>
                </div>
                <div className='Overview'>
                 <h1  className="overview_detais">Overview</h1>
-             <p className='overivewp'>{props. Overview}</p>
+             <p className='overivewp'>{props.Overview}</p>
                </div>
             </div>
             <div className='detail_img1'>
-            <img className='detail_imge' src={props. Homepage} alt="cardimg"></img>
+            <img className='detail_imge' src={props.Homepage} alt="cardimg"></img>
             </div>
         </div>
     </div>
